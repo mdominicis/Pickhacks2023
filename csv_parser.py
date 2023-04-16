8cc22e7eca92b0acec7afe2e1a0803004dd6825a
Dheeraj9489 <dheerajmb19@gmail.com>
2023-04-16 00:59:54 -0500

    A big update.
    All changes from Matthew and Dheeraj

--- csv_parser.py
+++ csv_parser.py
@@ -9,29 +9,32 @@ def get_field_values(fieldname):
             values.append(row[fieldname])
     return np.array(values)
 
-def get_car_details(index):
+def get_car_details(car_makes, car_models, car_years, car_emissions, index):
 
     car_details = []
-
-    #car makes
-    car_makes = get_field_values('make')
     car_details.append(car_makes[index])
-
-    #car models
-    car_models = get_field_values('model')
     car_details.append(car_models[index])
-
-    #car year
-    car_years = get_field_values('year')
     car_details.append(car_years[index])
-
-    car_emissions = get_field_values('co2TailpipeGpm')
     car_details.append(car_emissions[index])
     
-    return car_details
+    return np.array(car_details)
+
+# gets index if you put a make, model and year of a car
+def get_index(make, model, year, car_makes, car_models, car_years):
+    
+    for i in range(len(car_makes)):
+        if(make == car_makes[i]) and (model == car_models[i]) and (year == car_years[i]):
+            index = i
+    return index
 
 
 if __name__ == '__main__':
     # only runs if this file is run
-    car = get_car_details(0)
+
+    car_makes = get_field_values('make')
+    car_models = get_field_values('model')
+    car_years = get_field_values('year')
+    car_emissions = get_field_values('co2TailpipeGpm')
+
+    car = get_car_details(car_makes, car_models, car_years, car_emissions, 0)
     print(car)
--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import distancegetter
+import csv_parser
+import random
 
+origin = input("Enter Start Destination: ")
+destination = input("Enter End Destination: ")
+api_key = 'AIzaSyDc6_OK5NMUMTiK3r-WsOkTzuImskrCSRc'
+distance = distancegetter.get_distance(origin, destination, api_key)
+print(distance)
+print("\n")
+car_makes = csv_parser.get_field_values('make')
+car_models = csv_parser.get_field_values('model')
+car_years = csv_parser.get_field_values('year')
+car_emissions = csv_parser.get_field_values('co2TailpipeGpm')
+
+print("example:", csv_parser.get_car_details(car_makes, car_models, car_years, car_emissions, random.randint(0, len(car_makes) - 1))) # for reference
+make = input("Car's Make: ")
+model = input("Car's model: ")
+year = input("Car's year: ")
+
+index = csv_parser.get_index(make, model, year, car_makes, car_models, car_years)
+car_details = csv_parser.get_car_details(car_makes, car_models, car_years, car_emissions, index)
+
+print(car_details[1] + " " + str(car_details[2]) + "'s carbon emissions are " + str(car_details[3]) + " g/mi")
+
+print("Total carbon emissions: " + str(car_details[3] * distance) + " grams")
